899041221f1df4d6359bde9da20c7cce503bb5b8
jaime.quispe <jaime.quispe@despegar.com>
2020-08-11 12:24:54 -0300

    wip

--- src/main/java/implementation/ApiLocationMock.java
+++ src/main/java/implementation/ApiLocationMock.java
@@ -1,8 +1,8 @@
 package implementation;
 
-import com.google.common.collect.Lists;
 import model.Coordinate;
 import model.LocationConnector;
+import java.util.Arrays;
 import java.util.List;
 
 public class ApiLocationMock implements LocationConnector {
@@ -28,9 +28,9 @@ public class ApiLocationMock implements LocationConnector {
 
     @Override
     public List<Coordinate> getData(Coordinate arrival, Coordinate departure) {
-        List<Coordinate> resultOne = Lists.newArrayList(new Coordinate(11, 22), new Coordinate(15, 23), new Coordinate(33, 44));
-        List<Coordinate> resultTwo = Lists.newArrayList();
-        List<Coordinate> resultThree = Lists.newArrayList(new Coordinate(22, 66));
+        List<Coordinate> resultOne = Arrays.asList(new Coordinate(11, 22), new Coordinate(15, 23), new Coordinate(33, 44));
+        List<Coordinate> resultTwo = Arrays.asList();
+        List<Coordinate> resultThree = Arrays.asList(new Coordinate(22, 66));
 
         if (arrival.equals(new Coordinate(11, 22)) && departure.equals(new Coordinate(33, 44))) {
             return resultOne;
